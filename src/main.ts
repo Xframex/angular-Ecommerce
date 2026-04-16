25fc6ef53d6edf1cf8ac36da1002c679e6f98782
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-17 00:38:34 +0100

    routes configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideHttpClient } from '@angular/common/http';
+import { Routes, provideRouter } from '@angular/router';
+import { ProductList } from './app/components/product-list/product-list';
+
+// Define routes
+const routes: Routes = [
+  { path: 'category/:id', component: ProductList },
+  { path: 'category', component: ProductList },
+  { path: 'products', component: ProductList },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }
+];
 
 bootstrapApplication(App, {
   providers: [
-    provideHttpClient()
+    provideHttpClient(),
+    provideRouter(routes) // Provide the router with the defined routes
   ]
 });
