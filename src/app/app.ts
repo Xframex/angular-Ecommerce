c183297760c24816b8a9dee4c9df4f5d0b8490e3
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-17 02:08:35 +0100

    router establish

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { ProductList } from "./components/product-list/product-list";
 
 @Component({
   selector: 'app-root',
-  imports: [ ProductList, RouterOutlet],
+  imports: [ProductList, RouterOutlet, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -2,33 +2,45 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { CommonModule, CurrencyPipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CurrencyPipe,CommonModule],
+  imports: [CurrencyPipe, CommonModule],
   templateUrl: './product-list-grid.html',
-  //templateUrl: './product-list-table.html',
-   //templateUrl: './product-list-.html',
   styleUrl: './product-list.css',
 })
 export class ProductList implements OnInit {
+  products!: Product[];
+  currentCategoryId: number = 1;
 
-  products: Product[] = [];
-  
   // Inject the ProductService
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private router: ActivatedRoute,
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
     this.listProducts();
+    // check if "id" parameter is available in the route and update currentCategoryId accordingly
+    const hasCategoryId: boolean = this.router.snapshot.paramMap.has('id');
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.router.snapshot.paramMap.get('id')!;
+    }
+    else {
+      this.currentCategoryId = 1;
+    }
+
+    // Subscribe to the paramMap observable to react to changes in the route parameters
+    this.router.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
   // Method to fetch the list of products from the service
   listProducts() {
-    // Call the getProductList method of the ProductService and subscribe to the Observable
-    this.productService.getProductList().subscribe(
-      data => {
-        this.products = data;
-      }
-    );
+    // get products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe((data) => {
+      this.products = data;
+    });
   }
-
 }
