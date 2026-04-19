23fdc354e607c91c38f9779c9fac193481cc213e
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-19 03:30:17 +0100

    Add ProductCategoryMenu component import and update app.ts imports to include ProductCategoryMenu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 import { ProductList } from "./components/product-list/product-list";
+import { ProductCategoryMenu } from "./components/product-category-menu/product-category-menu";
 
 @Component({
   selector: 'app-root',
-  imports: [ProductList, RouterOutlet, RouterLinkWithHref, RouterLinkActive],
+  imports: [ProductList, RouterOutlet, RouterLinkWithHref, RouterLinkActive, ProductCategoryMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
