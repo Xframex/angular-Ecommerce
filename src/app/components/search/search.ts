5ba0c11156ea6d0fddec6bf4972e8e60822030b8
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-21 03:01:36 +0100

    Add doSearch method and router dependency to Search component in search.ts to handle search actions and navigate to search results page

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,14 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
-
   imports: [CommonModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
 export class Search {
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  // Method to handle search action and navigate to the search results page
+  doSearch(value: string): void {
+    console.log(`value is ${value}`);
+    this.router.navigateByUrl(`/search/${value}`);
+  }
 
 }
