c841982b5344699dbc8b73009e5f7bb2d72b8b31
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-21 01:47:08 +0100

    Implement Search class in search.ts with Angular component decorator and CommonModule import

---
+++ src/app/components/search/search.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-search',
+
+  imports: [CommonModule],
+  templateUrl: './search.html',
+  styleUrl: './search.css',
+})
+export class Search {
+
+}
