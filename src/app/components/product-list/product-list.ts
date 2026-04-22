af6d19b67f2392791fc291b871bb95e51eff2dfe
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-22 22:56:40 +0100

    Introduce searchMode flag and handleSearchProducts method in product-list.ts to enable product searching by keyword

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -13,16 +13,17 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductList implements OnInit {
   products!: Product[];
   currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
   // Inject the ProductService
   constructor(
     private productService: ProductService,
-    private router: ActivatedRoute,
+    private route: ActivatedRoute,
   ) {}
 
   ngOnInit(): void { 
     this.listProducts();
-    this.router.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe(params => {
     
     
     const id = params.get('id');
@@ -34,9 +35,51 @@ export class ProductList implements OnInit {
   }
   // Method to fetch the list of products from the service
   listProducts() {
-    // get products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe((data) => {
-      this.products = data;
-    });
+
+    // check if "keyword" parameter is available
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProducts() {
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )    
   }
 }
+
