f3baf7c2475daeef87ab7a179e6a95c783776558
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-27 02:09:35 +0100

    Add CartDetails class with listCartDetails method to display cart items in cart-details.html

--- src/app/components/cart-details/cart-details.ts
+++ src/app/components/cart-details/cart-details.ts
@@ -1,7 +1,7 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartItem } from '../../common/cart-item';
 import { CartService } from '../../services/cart.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-details',
@@ -9,9 +9,38 @@ import { CartService } from '../../services/cart.service';
   templateUrl: './cart-details.html',
   styleUrl: './cart-details.css',
 })
-export class CartDetails {
+export class CartDetails implements OnInit {
 
-  constructor(public cartService: CartService) { }
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) {
+
+   }
+
+   ngOnInit(): void {
+    this.listCartDetails();
+     
+   }
+  listCartDetails() {
+    // get the cart items from the service
+    this.cartItems = this.cartService.cartItems;
+
+    // compute totals based on the cart data that is in the service
+    this.cartService.computeCartTotals();
+
+    // subscribe to the cart totalPrice and totalQuantity
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+  }
+
 
 
 
