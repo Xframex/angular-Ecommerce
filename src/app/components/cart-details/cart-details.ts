265d26ae7f9ec33e151513a52807a95129da018a
ismail <125493050+Xframex@users.noreply.github.com>
2026-04-27 01:44:11 +0100

    Implement CartDetails component constructor in cart-details.ts with CartService injection for cart item management

--- src/app/components/cart-details/cart-details.ts
+++ src/app/components/cart-details/cart-details.ts
@@ -1,12 +1,18 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { CartItem } from '../../common/cart-item';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './cart-details.html',
   styleUrl: './cart-details.css',
 })
 export class CartDetails {
-  
+
+  constructor(public cartService: CartService) { }
+
+
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,6 +6,8 @@ import { Subject } from 'rxjs';
   providedIn: 'root',
 })
 export class CartService {
+  
+  
   constructor() {}
 
   cartItems: CartItem[] = [];
